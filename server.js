30ec1243938d5d6d63ea95a2fb49ffc1dfef1343
Christian Bryant <christian.bryant@mobelux.com>
2017-10-13 16:07:09 -0400

    homepage edits

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use("/static/css",express.static(__dirname + "/static/css"));
 app.use("/static/scss",express.static(__dirname + "/static/scss"));
 app.use("/static/js",express.static(__dirname + "/static/js"));
 app.use("/static/img",express.static(__dirname + "/static/img"));
+app.use("/static/img/hotels",express.static(__dirname + "/static/img/hotels"));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
@@ -41,7 +42,6 @@ app.get('/product-detail', function (req, res) {
 app.get('/contact', function (req, res) {
   res.sendFile(__dirname + '/contact.html');
 })
-
 
 app.listen(process.env.PORT || 8083, function () {
   console.log('Listening on port 8083!')
